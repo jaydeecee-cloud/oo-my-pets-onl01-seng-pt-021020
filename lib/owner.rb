65b48cec517f7d79b2d3647c3cc463f6eac3c0da
Joseph Choi <jdc07@live.com>
2020-04-15 16:31:09 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,7 +33,7 @@ class Owner
   end
 
   def walk_dogs
-  self.dogs.each {|dog| dog.mood = "happy"}
+    self.dogs.each {|dog| dog.mood = "happy"}
   end
 
   def feed_cats
@@ -45,13 +45,22 @@ class Owner
       self.cats.delete(cat)
       cat.owner = nil
       cat.mood = "nervous"
+    end
     self.dogs.each do |dog|
       self.dogs.delete(dog)
       dog.owner = nil
       dog.mood = "nervous"
+    end
+  end
+  
   def list_pets
     number_of_dogs = self.dogs.count
     number_of_cats = self.cats.count
+    "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+  end
+  
+  def self.reset_all
+    self.all.clear
   end
 end
 
